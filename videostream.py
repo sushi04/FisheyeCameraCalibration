5c204e59b071a0870429720db4f4d8699485482c
Sushil04 <129103555+sushi04@users.noreply.github.com>
2024-04-17 16:44:30 +0200

    videostream.py

--- videostream.py
+++ videostream.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 import os
+import datetime
 
-calibration_data = np.load("calibration_data.npz")#load calibration data saved after calibration
+calibration_data = np.load("calibration_data_1.npz")#load calibration data saved after calibration
 camera_matrix = calibration_data['camMatrix'] #read camera matrix
 dist_coeffs = calibration_data['coeffs'] #read dist_coeffs
 DIM = calibration_data["DIM"] #read dimensiions of image on which calbration was done
@@ -16,7 +17,7 @@ if not cap.isOpened():         #check if video is captured
     print("Error could not open video stream")
     exit()
 
-def undistort(camera_matrix , dist_coeffs, DIM):
+def undistort(camera_matrix , dist_coeffs, DIM,balance):
     dim2 = None
     dim3 = None
     
@@ -27,20 +28,14 @@ def undistort(camera_matrix , dist_coeffs, DIM):
             break
         
         try:
-            dim1 = frame.shape[:2][::-1]
-            assert dim1[0]/dim1[1] == DIM[0]/DIM[1]
-            if not dim2:
-                dim2 = dim1
-            if not dim2:
-                dim3 = dim1
-            #Scale calibration matrix as per aspect ratio
-            scaled_K = camera_matrix * dim1[0]/DIM[1]
-            scaled_K[2][2] = 1.0
-            # Undstort the image 
-            new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K,dist_coeffs,dim2,np.eye(3),balance=0.0)
-            map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, dist_coeffs, np.eye(3), camera_matrix, DIM, cv2.CV_16SC2)
+        
+            font = cv2.FONT_HERSHEY_PLAIN
+            map1, map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist_coeffs, np.eye(3),camera_matrix,DIM, cv2.CV_16SC2)
             undistorted_frame = cv2.remap(frame, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode= cv2.BORDER_CONSTANT)
+            date_time = str(datetime.datetime.now()) + str('   ')+str('Streaming camera id 001')
+            undistorted_frame = cv2.putText(undistorted_frame,date_time,(100,700),font,2,(255,255,0),2,cv2.LINE_4)
             #Display undistorted image
+            cv2.imshow('Original',frame)
             cv2.imshow('Undistorted Video', undistorted_frame)
             key = cv2.waitKey(1)
             # Exit key for program
@@ -51,4 +46,4 @@ def undistort(camera_matrix , dist_coeffs, DIM):
     
     cap.release()
 
-undistort(camera_matrix,dist_coeffs,DIM)
+undistort(camera_matrix,dist_coeffs,DIM,0.8)
