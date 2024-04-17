ef4314bb83c01776838c9a2a3c1a5a1df61d79ec
Sushil04 <129103555+sushi04@users.noreply.github.com>
2024-04-17 09:55:35 +0200

    video_calibration.py

--- video_calibration.py
+++ video_calibration.py
@@ -1,20 +1,33 @@
 import cv2
 import os
 import numpy as np
+import glob
 
-rtsp_link =''
-output_dir =''
+
+rtsp_link ='rtsp://10.8.0.12:8554/h264'
+output_dir ='/home/userpa/Desktop/calibration'
+
 os.makedirs(output_dir, exist_ok=True)
+os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = "video_codec"
 
-video_capture = cv2.VideoCapture(rtsp_link) # capture the video from rtsp_link
+camId = input('Enter camera id:')
 
-if not video_capture.isOpened():         #check if video is captured
+cap = cv2.VideoCapture(rtsp_link) # capture the video from rtsp_link
+fps = 25
+if not cap.isOpened():         #check if video is captured
     print("Error could not open video stream")
     exit()
-    
+
+
+#cap.set(cv2.CAP_PROP_FPS,fps)
+
+#actual_fps = cap.get(cv2.CAP_PROP_FPS)
+#print("Actual frame rate:", actual_fps)
+
+
 frame_count = 0                          #count no. of frames 
 while True:
-    ret, frame = video_capture.read()
+    ret, frame = cap.read()
     if not ret:
         break
     
@@ -24,10 +37,10 @@ while True:
     
     frame_count+=1                                 
      
-    if frame_count >=100:                                             #stop after reading 100 frames
+    if frame_count >=250:                                             #stop after reading 100 frames
         break
     
-video_capture.release()
+cap.release()
 
 CHECKERBOARD = (5,7)
 subpix_criteria = (cv2.TermCriteria_MAX_ITER + cv2.TERM_CRITERIA_EPS,30,0.01)
@@ -36,26 +49,36 @@ calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHEC
 objp = np.zeros((1,CHECKERBOARD[0]*CHECKERBOARD[1],3), np.float32)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0],0:CHECKERBOARD[1]].T.reshape(-1,2)
 
-img_shape = None
+_img_shape = None
 objpoints = []
 imgpoints = []
 
-for frame in frame_path:
-    img = cv2.imread(frame)
+images = glob.glob(os.path.join(output_dir,'*.jpg'))
+count =0   
+for img_path in images:
+    img = cv2.imread(img_path)
     
     if _img_shape == None:
         _img_shape = img.shape[:2]
     else:
         assert _img_shape == img.shape[:2]
-        
+  
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print('done')
     ret,corners = cv2.findChessboardCorners(gray,CHECKERBOARD,cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    print(ret)
     
     if ret == True:
         objpoints.append(objp)
         corner2 = cv2.cornerSubPix(gray, corners,(3,3),(-1,-1),subpix_criteria)
         imgpoints.append(corner2)
+        count+= 1
         
+    else:
+        os.remove(img_path)
+
+print(count)        
+
 N_OK = len(objpoints)
 camMatrix = np.zeros((3,3))
 coeffs = np.zeros((4,1))
@@ -65,4 +88,7 @@ tvecs = [np.zeros((1,1,3), dtype=np.float64) for i in range(N_OK)]
 rms, camMatrix, coeffs, rvecs, tvecs = cv2.fisheye.calibrate(objpoints,imgpoints,gray.shape[::-1],camMatrix,coeffs,rvecs,tvecs,calibration_flags,(cv2.TermCriteria_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6))
 DIM = _img_shape[::-1]
 
-np.savez("calibration_data.npz", camMatrix=camMatrix, coeffs=coeffs, DIM= DIM)
+print(camMatrix)
+print(coeffs)
+
+np.savez("calibration_data_{camId}.npz", camMatrix=camMatrix, coeffs=coeffs, DIM= DIM)
